e61bdf4c1945e8cf6539f534ca020dbd2fc3c5c3
Pedro Jardim <pedro200219@outlook.com>
2021-09-19 18:13:48 -0300

    Desafio finalizado

--- src/App.js
+++ src/App.js
@@ -1,29 +1,113 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [techs, setTechs] = useState("");
+
+  useEffect(() => {
+    api.get("repositories").then((response) => setRepositories(response.data));
+  }, []);
+
+  async function handleAddRepository(event) {
+    event.preventDefault();
+    let listTechs = techs.split(" ");
+    let data = {
+      title,
+      url,
+      techs: listTechs,
+    };
+    await api
+      .post("repositories", data)
+      .then((response) => setRepositories((prev) => [...prev, response.data]));
+    setTechs("");
+    setTitle("");
+    setUrl("");
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    const updatedRepositories = repositories.filter(
+      (repository) => repository.id !== id
+    );
+    setRepositories(updatedRepositories);
+  }
+
+  async function handleAddLike(id) {
+    const response = await api.post(`repositories/${id}/like`);
+    const likeRepository = response.data;
+    setRepositories(
+      repositories.map((repository) => {
+        return repository.id === id ? likeRepository : repository;
+      })
+    );
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li
+            style={{ background: "#7159C1", color: "#fff", padding: 20 }}
+            key={repository.id}
+          >
+            <p>{repository.title}</p>
+            <ul className="repository-techs">
+              {repository.techs.map((tech) => (
+                <li className="tech" key={tech}>
+                  {tech}
+                </li>
+              ))}
+            </ul>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+            <button
+              style={{ background: "#58A6FF" }}
+              title="Likes"
+              onClick={() => handleAddLike(repository.id)}
+            >
+              <span role="img" aria-label="like">
+                👍
+              </span>{" "}
+              {repository.likes}
+            </button>
+          </li>
+        ))}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <form onSubmit={handleAddRepository}>
+        <label>
+          Título
+          <input
+            type="text"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+            required
+          />
+        </label>
+        <label>
+          Url
+          <input
+            type="text"
+            value={url}
+            onChange={({ target }) => setUrl(target.value)}
+            required
+          />
+        </label>
+        <label>
+          Techs
+          <textarea
+            placeholder="Separe as techs por espaços"
+            value={techs}
+            onChange={({ target }) => setTechs(target.value)}
+            required
+          />
+        </label>
+        <button>Adicionar</button>
+      </form>
     </div>
   );
 }
